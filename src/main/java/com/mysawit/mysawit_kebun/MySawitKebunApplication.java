7e768a77f610f78af0f7b1877cf69ee6c14498e3
NaviCo-de <aridarrell30@gmail.com>
2026-02-17 23:21:24 +0700

    Early Setup

---
+++ src/main/java/com/mysawit/mysawit_kebun/MySawitKebunApplication.java
@@ -0,0 +1,13 @@
+package com.mysawit.mysawit_kebun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySawitKebunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySawitKebunApplication.class, args);
+	}
+
+}
