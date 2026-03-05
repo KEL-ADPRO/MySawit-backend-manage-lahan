a0ebc8c26a7d3f701f95faae3aace60b110f7d6c
Samuel Marcelino Tindaon <samuelmarcelinotindaon@gmail.com>
2026-03-05 16:41:25 +0700

    Updated Model

---
+++ src/main/java/com/mysawit/mysawit_kebun/model/Area.java
@@ -0,0 +1,40 @@
+package com.mysawit.mysawit_kebun.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+public class Area {
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "x", column = @Column(name = "bottom_left_x")),
+            @AttributeOverride(name = "y", column = @Column(name = "bottom_left_y"))
+    })
+    private Koordinat bottomLeft;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "x", column = @Column(name = "bottom_right_x")),
+            @AttributeOverride(name = "y", column = @Column(name = "bottom_right_y"))
+    })
+    private Koordinat bottomRight;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "x", column = @Column(name = "top_right_x")),
+            @AttributeOverride(name = "y", column = @Column(name = "top_right_y"))
+    })
+    private Koordinat topRight;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "x", column = @Column(name = "top_left_x")),
+            @AttributeOverride(name = "y", column = @Column(name = "top_left_y"))
+    })
+    private Koordinat topLeft;
+}
