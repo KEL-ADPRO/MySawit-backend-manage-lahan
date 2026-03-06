00d3faa4fe7fd487698cd65ca4f9911afc2c7d2f
Samuel Marcelino Tindaon <samuelmarcelinotindaon@gmail.com>
2026-03-06 18:19:28 +0700

    removed unused imports

--- src/main/java/com/mysawit/mysawit_kebun/controller/KebunController.java
+++ src/main/java/com/mysawit/mysawit_kebun/controller/KebunController.java
@@ -3,7 +3,6 @@ package com.mysawit.mysawit_kebun.controller;
 import com.mysawit.mysawit_kebun.model.Kebun;
 import com.mysawit.mysawit_kebun.service.KebunService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
