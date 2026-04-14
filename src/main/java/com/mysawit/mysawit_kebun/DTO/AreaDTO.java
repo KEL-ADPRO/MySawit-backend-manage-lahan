e06d9bddbdc9b614cb1f96f32ed7996363371d73
Samuel Marcelino Tindaon <samuelmarcelinotindaon@gmail.com>
2026-04-14 19:36:11 +0700

    Added methods to avoid long method chains in service

--- src/main/java/com/mysawit/mysawit_kebun/DTO/AreaDTO.java
+++ src/main/java/com/mysawit/mysawit_kebun/DTO/AreaDTO.java
@@ -1,5 +1,6 @@
 package com.mysawit.mysawit_kebun.DTO;
 
+import com.mysawit.mysawit_kebun.model.Area;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,4 +13,13 @@ public class AreaDTO {
     private KoordinatDTO bottomRight;
     private KoordinatDTO topRight;
     private KoordinatDTO topLeft;
+
+    public Area toEntity() {
+        return new Area(
+                this.bottomLeft.toEntity(),
+                this.bottomRight.toEntity(),
+                this.topRight.toEntity(),
+                this.topLeft.toEntity()
+        );
+    }
 }
--- src/main/java/com/mysawit/mysawit_kebun/DTO/KoordinatDTO.java
+++ src/main/java/com/mysawit/mysawit_kebun/DTO/KoordinatDTO.java
@@ -1,5 +1,6 @@
 package com.mysawit.mysawit_kebun.DTO;
 
+import com.mysawit.mysawit_kebun.model.Koordinat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,4 +11,8 @@ import lombok.NoArgsConstructor;
 public class KoordinatDTO {
     private int x;
     private int y;
+
+    public Koordinat toEntity() {
+        return new Koordinat(this.x, this.y);
+    }
 }
