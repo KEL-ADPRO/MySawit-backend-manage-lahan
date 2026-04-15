15d08e660ef14e9527008566d9e02998788fda57
Samuel Marcelino Tindaon <samuelmarcelinotindaon@gmail.com>
2026-04-15 23:31:51 +0700

    Added supir reference in kebun

--- src/main/java/com/mysawit/mysawit_kebun/model/Kebun.java
+++ src/main/java/com/mysawit/mysawit_kebun/model/Kebun.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -26,4 +28,9 @@ public class Kebun {
 
     @Column
     private String mandorId;
+
+    @ElementCollection
+    @CollectionTable(name = "supirKebun", joinColumns = @JoinColumn(name = "kebunId"))
+    @Column
+    private List<String> supirIds = new ArrayList<>();
 }
