6210030bd00cb66e4631c08ae558722ec1926795
Samuel Marcelino Tindaon <samuelmarcelinotindaon@gmail.com>
2026-05-22 22:33:38 +0700

    Added area calculation and tests

--- src/main/java/com/mysawit/mysawit_kebun/model/Area.java
+++ src/main/java/com/mysawit/mysawit_kebun/model/Area.java
@@ -39,4 +39,15 @@ public class Area {
             @AttributeOverride(name = "y", column = @Column(name = "top_left_y"))
     })
     private Koordinat topLeft;
+
+    public double getCalculatedArea() {
+        if (bottomLeft == null || bottomRight == null || topRight == null || topLeft == null) {
+            return 0.0;
+        }
+        double sum = ((double) bottomLeft.getX() * bottomRight.getY() - (double) bottomLeft.getY() * bottomRight.getX())
+                   + ((double) bottomRight.getX() * topRight.getY() - (double) bottomRight.getY() * topRight.getX())
+                   + ((double) topRight.getX() * topLeft.getY() - (double) topRight.getY() * topLeft.getX())
+                   + ((double) topLeft.getX() * bottomLeft.getY() - (double) topLeft.getY() * bottomLeft.getX());
+        return 0.5 * Math.abs(sum);
+    }
 }
